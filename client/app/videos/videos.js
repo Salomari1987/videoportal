b81b6d25701916849aa5fff780bf9b056f299afd
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-12-13 08:06:34 +0400

    implement lazy loading and pause on play

--- client/app/videos/videos.js
+++ client/app/videos/videos.js
@@ -1,36 +1,65 @@
-angular.module('co.videos', ["ngSanitize"])
+angular.module('co.videos', ["ngSanitize", "infinite-scroll", "com.2fdevs.videogular"])
 
-.controller('VideosController', function ($sce, $scope, $window, $location, Videos, HelperFuncs) {
+.controller('VideosController', function ($scope, LazyLoading) {
   $scope.data = {};
   $scope.max = 5;
   $scope.isReadonly = false;
+  $scope.data = new LazyLoading();
+  $scope.players = [];
 
+  $scope.onPlayerReady = function (API, index) {
+      
+      $scope.players[index] = API;
+  };
+  $scope.onUpdateState = function (state, index) {
+            if (state === 'play') {
+                // pause other players
+                for (var i=0, l=this.players.length; i<l; i++) {
+                    if (i !== index) {
+                        $scope.players[i].pause();
+                    }
+                }
+            }
+        };
   $scope.hoveringOver = function(value) {
     $scope.overStar = value;
     $scope.percent = 100 * (value / $scope.max);
   };
 
   $scope.rateVideo = function (id, value) {
-    console.log(id, value)
     Videos.rateVideo({videoId:id, rating:value})
-    .then(function (resp) {
-      console.log(resp)
-    })
+  };
+})
+.factory('LazyLoading', function($sce, HelperFuncs, Videos) {
+  var LazyLoading = function() {
+    this.videos = [];
+    this.busy = false;
+    this.skip = 0
+    this.limit = 10;
   };
 
-  // get first 10 videos by default
-  // attach array to scope data object to iterate over it
-  Videos.getMany()
-  .then(function (resp) {
-    var videos = resp.map(function (e) { return e;});
-    for (var i = 0; i < videos.length; i++) {
-      videos[i].overallRating = Math.round(HelperFuncs.arrayAverage(videos[i].ratings));
-      var videoType = HelperFuncs.videoType(videos[i].url);
-      videos[i].source = {src:$sce.trustAsResourceUrl(videos[i].url), type: "video/"+videoType }
-    }
-    return videos;
-  })
-  .then(function (videos) {
-    $scope.data.videos = videos;
-  })
+  LazyLoading.prototype.loadMore = function() {
+    var videoType;
+    if (this.busy) return;
+    this.busy = true;
+    
+    var that = this;
+    Videos.getMany(this.skip, this.limit)
+    .then(function (resp) {
+      var videos = resp;
+      for (var i = 0; i < videos.length; i++) {
+        videos[i].overallRating = Math.round(HelperFuncs.arrayAverage(videos[i].ratings));
+        var videoType = HelperFuncs.videoType(videos[i].url);
+        videos[i].source = [{src:$sce.trustAsResourceUrl(videos[i].url), type: "video/"+videoType }]
+        videos[i].theme = {
+          url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+        };
+        that.videos.push(videos[i]);
+      }
+      that.skip = that.videos.length;
+      that.busy = false;
+
+    });
+  };
+  return LazyLoading;
 });
