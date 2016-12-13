5c36cc82c147fb700436f0274be07085b38c08d9
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-12-13 09:48:18 +0400

    Restyle video detail page

--- client/app/video/video.js
+++ client/app/video/video.js
@@ -2,8 +2,9 @@ angular.module('co.video', [
       "ngSanitize",
       "com.2fdevs.videogular"])
 .controller('VideoController',
-    function ($sce, $stateParams, Videos, $scope) {
-      $scope.video = {}
+    function ($sce, $stateParams, Videos, $scope, HelperFuncs) {
+      $scope.video = {};
+      $scope.max = 5;
       Videos.getOne($stateParams.id)
       .then(function(video) {
         $scope.video = video;
@@ -13,6 +14,8 @@ angular.module('co.video', [
         $scope.video.theme = {
           url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
         };
+        $scope.video.overallRating = Math.round(HelperFuncs.arrayAverage(video.ratings))
+        $scope.video.videoType = HelperFuncs.videoType(video.videoType);
       })
      .catch(function(error) {
        console.error(error);
